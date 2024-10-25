988fa6eb1d5af464f0030b305bc433ebc0085eb7
Shahzad <shazee257@gmail.com>
2024-10-25 19:04:04 +0500

    fixed EmailServiceType issue (constructor parameters)

--- src/types.ts
+++ src/types.ts
@@ -4,20 +4,15 @@ export interface bindEmailDataType {
 }
 
 export interface sendEmailType {
-    html?: string | undefined;
-    templateRef: string;
-    data: any;
     to: string;
     subject: string;
+    html?: string | undefined;
+    templateRef?: string;
+    data?: any;
 }
 
 export interface EmailServiceType {
     emailUser: string;
     emailPassword: string;
     emailTemplatesDirectory: string;
-
-    bindEmailData({ data, fileString }: bindEmailDataType): string;
-    generateHTMLString(type: string, data: any): string;
-    sendMail({ html, templateRef, data, to, subject }: sendEmailType): Promise<void>;
-    replaceAll(str: string, find: string, replace: string): string;
 }
